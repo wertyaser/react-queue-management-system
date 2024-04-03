4317a7f3c98c122f4d3a4ec540b3cbeeb3f8baa0
wertyaser <nacmanleonielgarin@gmail.com>
2024-04-03 13:46:53 +0800

    deleted handleLogin server js needs update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,24 +4,10 @@ import axios from "axios";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    try {
-      const response = await axios.post("http://localhost:3000/login", {
-        username,
-        password,
-      });
-      if (response.data.success) {
-        // Redirect to dashboard or perform any action upon successful login
-        setMessage(response.data.message);
-      } else {
-        setMessage(response.data.message);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
+    //burat
   };
 
   return (
@@ -54,7 +40,6 @@ const Login = () => {
         >
           Submit
         </button>
-        {message && <p>{message}</p>}
       </form>
     </div>
   );
